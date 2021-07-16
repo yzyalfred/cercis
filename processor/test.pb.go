acf70010a8cc270128b1bb8a6b0a34ca71ac1f26
yzyalfred <344718275@qq.com>
2021-07-16 11:03:57 +0800

    time

--- processor/test.pb.go
+++ processor/test.pb.go
@@ -67,6 +67,52 @@ func (m *RpcSayHello) GetName() string {
 	return ""
 }
 
+// msgId: MSG_ID_TEST_PB 2
+type RpcTestPB struct {
+	Data []uint32 `protobuf:"varint,1,rep,packed,name=data,proto3" json:"data,omitempty"`
+}
+
+func (m *RpcTestPB) Reset()         { *m = RpcTestPB{} }
+func (m *RpcTestPB) String() string { return proto.CompactTextString(m) }
+func (*RpcTestPB) ProtoMessage()    {}
+func (*RpcTestPB) Descriptor() ([]byte, []int) {
+	return fileDescriptor_d27d27d958137878, []int{1}
+}
+func (m *RpcTestPB) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *RpcTestPB) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_RpcTestPB.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *RpcTestPB) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_RpcTestPB.Merge(m, src)
+}
+func (m *RpcTestPB) XXX_Size() int {
+	return m.Size()
+}
+func (m *RpcTestPB) XXX_DiscardUnknown() {
+	xxx_messageInfo_RpcTestPB.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_RpcTestPB proto.InternalMessageInfo
+
+func (m *RpcTestPB) GetData() []uint32 {
+	if m != nil {
+		return m.Data
+	}
+	return nil
+}
+
+// --------------------------------------------- s2c -------------------------------------------------------- //
 // msgId: MSG_ID_SAY_HELLO_REPLY 10001
 type RpcSayHelloReply struct {
 	Word string `protobuf:"bytes,1,opt,name=word,proto3" json:"word,omitempty"`
@@ -76,7 +122,7 @@ func (m *RpcSayHelloReply) Reset()         { *m = RpcSayHelloReply{} }
 func (m *RpcSayHelloReply) String() string { return proto.CompactTextString(m) }
 func (*RpcSayHelloReply) ProtoMessage()    {}
 func (*RpcSayHelloReply) Descriptor() ([]byte, []int) {
-	return fileDescriptor_d27d27d958137878, []int{1}
+	return fileDescriptor_d27d27d958137878, []int{2}
 }
 func (m *RpcSayHelloReply) XXX_Unmarshal(b []byte) error {
 	return m.Unmarshal(b)
@@ -113,22 +159,25 @@ func (m *RpcSayHelloReply) GetWord() string {
 }
 
 func init() {
-	proto.RegisterType((*RpcSayHello)(nil), "pb.rpcSayHello")
-	proto.RegisterType((*RpcSayHelloReply)(nil), "pb.rpcSayHelloReply")
+	proto.RegisterType((*RpcSayHello)(nil), "proto.rpcSayHello")
+	proto.RegisterType((*RpcTestPB)(nil), "proto.rpcTestPB")
+	proto.RegisterType((*RpcSayHelloReply)(nil), "proto.rpcSayHelloReply")
 }
 
 func init() { proto.RegisterFile("gs/test.proto", fileDescriptor_d27d27d958137878) }
 
 var fileDescriptor_d27d27d958137878 = []byte{
-	// 128 bytes of a gzipped FileDescriptorProto
+	// 156 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0xe2, 0x4d, 0x2f, 0xd6, 0x2f,
-	0x49, 0x2d, 0x2e, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x2a, 0x48, 0x52, 0x52, 0xe4,
-	0xe2, 0x2e, 0x2a, 0x48, 0x0e, 0x4e, 0xac, 0xf4, 0x48, 0xcd, 0xc9, 0xc9, 0x17, 0x12, 0xe2, 0x62,
-	0xc9, 0x4b, 0xcc, 0x4d, 0x95, 0x60, 0x54, 0x60, 0xd4, 0xe0, 0x0c, 0x02, 0xb3, 0x95, 0xd4, 0xb8,
-	0x04, 0x90, 0x94, 0x04, 0xa5, 0x16, 0xe4, 0x54, 0x82, 0xd4, 0x95, 0xe7, 0x17, 0xa5, 0xc0, 0xd4,
-	0x81, 0xd8, 0x4e, 0x12, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c,
-	0xe3, 0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x90, 0xc4, 0x06,
-	0xb6, 0xcf, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0xef, 0xc1, 0x82, 0x97, 0x80, 0x00, 0x00, 0x00,
+	0x49, 0x2d, 0x2e, 0xd1, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0x05, 0x53, 0x4a, 0x8a, 0x5c,
+	0xdc, 0x45, 0x05, 0xc9, 0xc1, 0x89, 0x95, 0x1e, 0xa9, 0x39, 0x39, 0xf9, 0x42, 0x42, 0x5c, 0x2c,
+	0x79, 0x89, 0xb9, 0xa9, 0x12, 0x8c, 0x0a, 0x8c, 0x1a, 0x9c, 0x41, 0x60, 0xb6, 0x92, 0x3c, 0x17,
+	0x67, 0x51, 0x41, 0x72, 0x48, 0x6a, 0x71, 0x49, 0x80, 0x13, 0x48, 0x41, 0x4a, 0x62, 0x49, 0xa2,
+	0x04, 0xa3, 0x02, 0xb3, 0x06, 0x6f, 0x10, 0x98, 0xad, 0xa4, 0xc6, 0x25, 0x80, 0x64, 0x46, 0x50,
+	0x6a, 0x41, 0x4e, 0x25, 0x48, 0x5d, 0x79, 0x7e, 0x51, 0x0a, 0xcc, 0x20, 0x10, 0xdb, 0x49, 0xe2,
+	0xc4, 0x23, 0x39, 0xc6, 0x0b, 0x8f, 0xe4, 0x18, 0x1f, 0x3c, 0x92, 0x63, 0x9c, 0xf0, 0x58, 0x8e,
+	0xe1, 0xc2, 0x63, 0x39, 0x86, 0x1b, 0x8f, 0xe5, 0x18, 0x92, 0xd8, 0xc0, 0x8e, 0x31, 0x06, 0x04,
+	0x00, 0x00, 0xff, 0xff, 0x7d, 0xcc, 0xdf, 0x32, 0xa4, 0x00, 0x00, 0x00,
 }
 
 func (m *RpcSayHello) Marshal() (dAtA []byte, err error) {
@@ -155,6 +204,47 @@ func (m *RpcSayHello) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.Name)
 		copy(dAtA[i:], m.Name)
 		i = encodeVarintTest(dAtA, i, uint64(len(m.Name)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *RpcTestPB) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *RpcTestPB) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *RpcTestPB) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if len(m.Data) > 0 {
+		dAtA2 := make([]byte, len(m.Data)*10)
+		var j1 int
+		for _, num := range m.Data {
+			for num >= 1<<7 {
+				dAtA2[j1] = uint8(uint64(num)&0x7f | 0x80)
+				num >>= 7
+				j1++
+			}
+			dAtA2[j1] = uint8(num)
+			j1++
+		}
+		i -= j1
+		copy(dAtA[i:], dAtA2[:j1])
+		i = encodeVarintTest(dAtA, i, uint64(j1))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -211,6 +301,22 @@ func (m *RpcSayHello) Size() (n int) {
 	l = len(m.Name)
 	if l > 0 {
 		n += 1 + l + sovTest(uint64(l))
+	}
+	return n
+}
+
+func (m *RpcTestPB) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	if len(m.Data) > 0 {
+		l = 0
+		for _, e := range m.Data {
+			l += sovTest(uint64(e))
+		}
+		n += 1 + sovTest(uint64(l)) + l
 	}
 	return n
 }
@@ -295,6 +401,132 @@ func (m *RpcSayHello) Unmarshal(dAtA []byte) error {
 			}
 			m.Name = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTest(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTest
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *RpcTestPB) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTest
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: rpcTestPB: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: rpcTestPB: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType == 0 {
+				var v uint32
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowTest
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					v |= uint32(b&0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				m.Data = append(m.Data, v)
+			} else if wireType == 2 {
+				var packedLen int
+				for shift := uint(0); ; shift += 7 {
+					if shift >= 64 {
+						return ErrIntOverflowTest
+					}
+					if iNdEx >= l {
+						return io.ErrUnexpectedEOF
+					}
+					b := dAtA[iNdEx]
+					iNdEx++
+					packedLen |= int(b&0x7F) << shift
+					if b < 0x80 {
+						break
+					}
+				}
+				if packedLen < 0 {
+					return ErrInvalidLengthTest
+				}
+				postIndex := iNdEx + packedLen
+				if postIndex < 0 {
+					return ErrInvalidLengthTest
+				}
+				if postIndex > l {
+					return io.ErrUnexpectedEOF
+				}
+				var elementCount int
+				var count int
+				for _, integer := range dAtA[iNdEx:postIndex] {
+					if integer < 128 {
+						count++
+					}
+				}
+				elementCount = count
+				if elementCount != 0 && len(m.Data) == 0 {
+					m.Data = make([]uint32, 0, elementCount)
+				}
+				for iNdEx < postIndex {
+					var v uint32
+					for shift := uint(0); ; shift += 7 {
+						if shift >= 64 {
+							return ErrIntOverflowTest
+						}
+						if iNdEx >= l {
+							return io.ErrUnexpectedEOF
+						}
+						b := dAtA[iNdEx]
+						iNdEx++
+						v |= uint32(b&0x7F) << shift
+						if b < 0x80 {
+							break
+						}
+					}
+					m.Data = append(m.Data, v)
+				}
+			} else {
+				return fmt.Errorf("proto: wrong wireType = %d for field Data", wireType)
+			}
 		default:
 			iNdEx = preIndex
 			skippy, err := skipTest(dAtA[iNdEx:])
--- utils/time.go
+++ utils/time.go
@@ -1,1 +1,11 @@
 package utils
+
+import "time"
+
+func NowSecond() int64  {
+	return time.Now().Unix()
+}
+
+func NowMillisecond() int64  {
+	return time.Now().UnixNano() / 1000000
+}
